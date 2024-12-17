3246c27c7ed103ca8bb522c1ec28021d2e2c786d
donsuth <donald@sutherland.id.au>
2024-12-17 17:42:37 +0800

    Refactor into class

--- main.py
+++ main.py
@@ -12,6 +12,42 @@ PORT = 80
 CONNECTIONS = set()
 
 
+class ConnectionManager:
+	def __init__(self):
+		self.HOST = "0.0.0.0"
+		self.PORT = 80
+		self.connections = set()
+		self.redis_connection: redis.client.Redis | None = None
+
+	async def run(self):
+		ssl_context = ssl.create_default_context()
+		redis_host = os.environ.get("REDIS_HOST", "localhost")
+		self.redis_connection = await redis.Redis(host=redis_host, port=6379, decode_responses=True)
+		async with self.redis_connection.pubsub() as pubsub:
+			await pubsub.subscribe("telepathy:json")
+			async with websockets.serve(
+					handler=register,
+					host=HOST,
+					port=PORT,
+					ssl=ssl_context,
+			):
+				await forward_redis_messages(pubsub=pubsub)
+
+	async def handle_connection(self, websocket: ServerConnection):
+		# Register
+		pass
+
+	async def check_credentials(self):
+		# Check credentials
+		pass
+
+	async def parse_received_json_message(self):
+		pass
+
+	async def forward_redis_messages(self):
+		pass
+
+
 async def register(websocket: ServerConnection):
 	credentials_valid = await check_credentials(websocket=websocket)
 	if credentials_valid:
